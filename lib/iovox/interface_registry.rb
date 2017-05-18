3cd911fa1e0aebc1c0e4b07c159a4d5cdbf86a70
Erwan Thomas <erwan@effilab-local.com>
2017-05-18 12:09:25 +0200

    fix circular dependency warning

--- lib/iovox/interface_registry.rb
+++ lib/iovox/interface_registry.rb
@@ -1,30 +1,32 @@
 # frozen_string_literal: true
 
-require 'iovox/all'
+require 'thread'
+require 'iovox/string_inflector'
 
-class Iovox::InterfaceRegistry
-  attr_reader :client
+module Iovox
+  class InterfaceRegistry
+    attr_reader :client
 
-  def initialize(client)
-    @client = client
-    @mutex = Mutex.new
-    @map = {}
-  end
+    def initialize(client)
+      @client = client
+      @mutex = Mutex.new
+      @map = {}
+    end
 
-  def [](key)
-    key = key.to_s
+    def [](key)
+      key = key.to_s
 
-    @mutex.synchronize do
-      @map[key] ||= interface_for(key)
+      @mutex.synchronize do
+        @map[key] ||= interface_for(key)
+      end
+    end
+
+    private
+
+    def interface_for(entity_name)
+      interface_name = "#{StringInflector.camel_case(entity_name)}Interface"
+
+      Iovox.const_get(interface_name).new(self)
     end
   end
-
-  private
-
-  def interface_for(entity_name)
-    interface_name = "#{Iovox::StringInflector.camel_case(entity_name)}Interface"
-
-    Iovox.const_get(interface_name).new(self)
-  end
 end
-

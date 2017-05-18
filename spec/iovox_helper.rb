69ba2524f1b88b7f868b6c53634c600fcc5e3e31
Erwan Thomas <erwan@effilab-local.com>
2017-05-18 11:45:57 +0200

    refactor spec/iovox helpers

---
+++ spec/iovox_helper.rb
@@ -0,0 +1,9 @@
+# frozen_string_literal: true
+
+require 'bundler/setup'
+require 'dotenv/load'
+require 'byebug'
+require 'awesome_print'
+
+require_relative 'support/client'
+require_relative 'support/sandbox_proxy'

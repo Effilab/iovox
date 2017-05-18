d62aab7e6c682f361184f7215c22d8ed5811dbab
Erwan Thomas <erwan@effilab-local.com>
2017-05-18 13:51:11 +0200

    silence warnings from 3rd-party libraries

--- spec/iovox_helper.rb
+++ spec/iovox_helper.rb
@@ -3,7 +3,8 @@
 require 'bundler/setup'
 require 'dotenv/load'
 require 'byebug'
-require 'awesome_print'
+require 'iovox/kernel'
+Iovox::Kernel.require 'awesome_print', verbose: false
 
 require_relative 'support/client'
 require_relative 'support/sandbox_proxy'

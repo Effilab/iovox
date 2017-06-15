21a077ba26e3f30bb7fe563d5cd5f2b15ac0754e
Erwan Thomas <erwan@effilab-local.com>
2017-06-15 11:30:37 +0200

    add CLI helpers

---
+++ lib/iovox/cli.rb
@@ -0,0 +1,84 @@
+# frozen_string_literal: true
+
+require 'optparse'
+
+module Iovox
+  module Cli
+    class Parser
+      attr_reader :optparser
+
+      def initialize(&block)
+        @optparser = OptionParser.new(&block)
+      end
+
+      def call(argv = ARGV)
+        begin
+          optparser.order!(argv)
+        rescue OptionParser::InvalidOption => error
+          argv.unshift(*error.args)
+        end
+
+        argv
+      end
+    end
+  end
+end
+
+options = {
+  proxy: false,
+  target: :dev,
+}
+
+Iovox::Cli::Parser.new do |opts|
+  opts.on('--[no-]proxy', "Proxy TCP sockets, default: #{options[:proxy]}") do |value|
+    options[:proxy] = value
+  end
+
+  opts.on('--target=NAME', "Target name (dev/prod), default: #{options[:target]}") do |value|
+    case value
+    when 'dev', 'prod'
+      options[:target] = value.to_sym
+    else
+      raise ArgumentError, 'Unknown value for --target argument'
+    end
+  end
+end.call
+
+case options[:target]
+when :prod
+  ENV['IOVOX_URL']        ||= ENV.fetch('PROD_IOVOX_URL')
+  ENV['IOVOX_USERNAME']   ||= ENV.fetch('PROD_IOVOX_USERNAME')
+  ENV['IOVOX_SECURE_KEY'] ||= ENV.fetch('PROD_IOVOX_SECURE_KEY')
+else
+  ENV['IOVOX_URL']        ||= ENV.fetch('DEV_IOVOX_URL')
+  ENV['IOVOX_USERNAME']   ||= ENV.fetch('DEV_IOVOX_USERNAME')
+  ENV['IOVOX_SECURE_KEY'] ||= ENV.fetch('DEV_IOVOX_SECURE_KEY')
+end
+
+if options[:proxy]
+  case options[:target]
+  when :prod
+    ENV['LOCAL_PROXY_SERVER'] ||= ENV['PROD_LOCAL_PROXY_SERVER']
+    ENV['LOCAL_PROXY_PORT'] ||= ENV['PROD_LOCAL_PROXY_PORT']
+  else
+    ENV['LOCAL_PROXY_SERVER'] ||= ENV['DEV_LOCAL_PROXY_SERVER']
+    ENV['LOCAL_PROXY_PORT'] ||= ENV['DEV_LOCAL_PROXY_PORT']
+  end
+end
+
+require 'iovox/all'
+
+Iovox::Client.configuration[:logger] = Iovox::Logger.new(
+  File.join('log', "#{options[:target]}.log")
+)
+
+if options[:proxy]
+  Iovox::Client.configuration[:socks_proxy] = {
+    server: ENV['LOCAL_PROXY_SERVER'],
+    port: ENV['LOCAL_PROXY_PORT'],
+  }
+end
+
+if options[:target] == :prod
+  Iovox::Client.configuration[:read_only] = true
+end
